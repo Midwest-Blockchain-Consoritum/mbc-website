b7321265263821fe699432b12df197348c4e9681
Zachary Thielemann <zachdt@gmail.com>
2019-12-28 16:06:45 -0800

    repaired router, added react-jss support and inital styling

--- src/views/navigation/Navigation.js
+++ src/views/navigation/Navigation.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Typography, AppBar, Toolbar } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+
+import {createUseStyles} from 'react-jss'
+import styles from '../../styles/navigation/Navigation'
+
+const useStyles = createUseStyles(styles)
 
 const Navigation = () => {
+  const classes = useStyles()
   return (
-    <div>
-      <p>Navigation</p>
+    <div className={classes.root}>
+      <AppBar position='static'>
+        <Toolbar>
+          <div className={classes.title}>
+            <Typography variant='h4'>
+              <Link to='/'>
+                Midwest Blockchain Consoritum
+              </Link>
+            </Typography>
+            </div>
+          <div>    
+          <Typography variant='h5'>
+            <Link to='/about'>
+              About
+            </Link>
+          </Typography>
+          </div>
+        </Toolbar>
+      </AppBar>
+      
     </div>
   )
 }
